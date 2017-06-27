cb82d701cf874e01eb86c4f08eaaa09f797ae422
Jérôme Avoustin <jerome@rehia.fr>
2017-06-27 18:42:35 +0200

    feat: whyat can track events and post them to an api like whyat-server

---
+++ gulpfile.babel.js
@@ -0,0 +1,13 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+
+gulp.task('build:es', () => {
+  gulp.src('src/**/*.js')
+    .pipe(babel({
+      babelrc: false,
+      presets: [
+        ['env', { modules: false }]
+      ]
+    }))
+    .pipe(gulp.dest('lib/es'));
+});
---
+++ test/tracker.js
@@ -0,0 +1,96 @@
+'use strict';
+
+import {expect, use} from 'chai';
+import {spy, match} from 'sinon';
+import sinonChai from 'sinon-chai';
+use(sinonChai);
+import {init} from '../src/tracker';
+
+describe('track event', () => {
+  var track, http, config;
+
+  beforeEach(() => {
+    http = {
+      post: () => Promise.resolve()
+    };
+    spy(http, 'post');
+
+    config = {
+      url: 'https://tracker.saagie.io/track/event',
+      application: 'mocha',
+      browser: {
+        appCodeName: 'Mozilla',
+        appName: 'Netscape',
+        appVersion: '5.0 Chrome/58.0',
+        platform: 'MacIntel',
+        userAgent: 'Mozilla/5.0 Chrome/58.0',
+      }
+    };
+
+    track = init(config, http.post);
+  });
+
+  const pageVisited = 'PAGE_VISITED';
+  const platform = 'test';
+  const user = {
+    id: '1453847392',
+    name: 'John Doe'
+  };
+  const uri = 'http://current.uri';
+  const event = {
+    type: pageVisited,
+    platform,
+    user,
+    payload: {},
+    uri
+  };
+
+  it('should track event', async () => {
+    event.payload = {
+      pageUrl: 'http://server.com/index.html'
+    };
+
+    await track(event);
+
+    expect(http.post).to.have.been.calledWithMatch(
+      'https://tracker.saagie.io/track/event', {
+        type: pageVisited,
+        applicationID: 'mocha',
+        platformID: 'test',
+        user: {id: '1453847392'},
+        payload: {pageUrl: 'http://server.com/index.html'},
+        browser: match({appCodeName: 'Mozilla'}),
+        uri: 'http://current.uri',
+        timestamp: match.number
+      });
+  });
+
+  it('should not raise an error when event has not been tracked', () => {
+    http.post = () => Promise.reject(new Error('server unreachable'));
+
+    track = init(config, http.post, () => {
+    });
+
+    event.payload = {
+      pageUrl: 'http://no-server.com/index.html'
+    };
+
+    expect(async () => await track(event))
+      .to.not.throw();
+  });
+
+  it('should log any error to keep track of it', async () => {
+    http.post = () => Promise.reject(new Error('server unreachable'));
+    const log = spy();
+
+    track = init(config, http.post, log);
+
+    event.payload = {
+      pageUrl: 'http://no-server.com/index.html'
+    };
+
+    await track(event);
+
+    expect(log).to.have.been.calledOnce;
+  });
+});
