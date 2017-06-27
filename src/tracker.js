89d61acf6611141ac6ca685689c16463419ed8b6
Jérôme Avoustin <jerome@rehia.fr>
2017-06-27 18:42:35 +0200

    feat: whyat can track events and post them to an api like whyat-server

---
+++ src/tracker.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import 'whatwg-fetch';
+
+const defaultLog = (...params) => console.log(...params);
+
+const defaultPost = (url, body) => fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(body)
+});
+
+export const init =
+  ({url, application, browser}, post = defaultPost, log = defaultLog) =>
+    async ({type, payload, platform, user:{id}, uri}) => {
+      try {
+        await post(url, {
+          applicationID: application,
+          platformID: platform,
+          user: {id},
+          type,
+          payload,
+          browser,
+          uri,
+          timestamp: Date.now()
+        });
+      } catch (error) {
+        log(`Error while logging event ${type} with values`, payload);
+      }
+    };
