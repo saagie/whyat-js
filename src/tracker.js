20efc616d3a9e8e48b494ce831a129b7a9083f0b
Guillaume Lours <guillaume@saagie.com>
2017-07-12 19:59:14 +0200

    feat : Add default tracking events

--- src/tracker.js
+++ src/tracker.js
@@ -13,30 +13,65 @@ const defaultPost = (url, body) => fetch(url, {
   body: JSON.stringify(body)
 });
 
+export const EventType = {
+  PAGE_VISITED: 'PAGE_VISITED',
+  LINK_CLICKED: 'LINK_CLICKED',
+  FORM_SUBMITTED: 'FORM_SUBMITTED'
+};
+
 const URL = t.maybe(t.String);
 const NotEmptyString = t.refinement(t.String, s => s.length > 0);
 
+const postEvent =
+  ({url, application, platform, browser}, post, log) =>
+      async ({type, payload, user: {id}, uri, platform: currentPlatform = platform}) => {
+        try {
+          await post(`${url}/event`, {
+            applicationID: application,
+            platformID: currentPlatform,
+            user: {id},
+            type,
+            payload,
+            browser,
+            uri,
+            timestamp: Date.now()
+          });
+        } catch (error) {
+          log(`Error while logging event ${type} with values`, payload);
+        }
+      };
+
+const tracker = (options, post, log) => ({
+  postEvent: postEvent(options, post, log),
+  pageViewed: (user, name = document.title, payload = {}, uri = document.location.href) => postEvent(options, post, log)({
+    type: EventType.PAGE_VISITED,
+    user,
+    uri,
+    payload: Object.assign({}, payload, {name})
+  }),
+  linkClicked: (user, name, payload = {}, uri = document.location.href) => postEvent(options, post, log)({
+    type: EventType.LINK_CLICKED,
+    user,
+    uri,
+    payload: Object.assign({},payload, {name})
+  }),
+  formSubmitted: (user, name, payload, uri = document.location.href) => postEvent(options, post, log)({
+    type: EventType.FORM_SUBMITTED,
+    user,
+    uri,
+    payload : Object.assign({},payload, {name})
+  })
+});
+
+const noop = (options, post, log) => ({
+    postEvent: (options, post, log) => {},
+    pageViewed:(user, name) => {},
+    linkClicked: (user, name, payload) => {}
+  });
+
 export const init =
-  ({url, application, browser}, post = defaultPost, log = defaultLog) =>
-    t.match(URL(url),
-        NotEmptyString, () => async ({type, payload, platform, user:{id}, uri}) => {
-            try {
-                await post(`${url}/event`, {
-                    applicationID: application,
-                    platformID: platform,
-                    user: {id},
-                    type,
-                    payload,
-                    browser,
-                    uri,
-                    timestamp: Date.now()
-                });
-            } catch (error) {
-                log(`Error while logging event ${type} with values`, payload);
-            }
-        },
-        t.Any, () => {
-                log("No server url defined");
-                return async () => void(0);
-            }
-      );
+  (options, post = defaultPost, log = defaultLog) =>
+    t.match(URL(options.url),
+      NotEmptyString, () => tracker(options, post, log),
+      t.Any, () => { log("No server url defined"); return noop(options, post, log);}
+    );
